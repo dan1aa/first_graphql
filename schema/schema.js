69a00ff7ad6ac09e2f83aa23bacab08d30f25b74
dan1aa <burmeidan@gmail.com>
2021-11-16 10:32:36 +0200

    [+] add files

--- schema/schema.js
+++ schema/schema.js
@@ -1,74 +1,74 @@
-const {
-    GraphQLString,
-     GraphQLList,
-      GraphQLObjectType,
-       GraphQLNonNull,
-        GraphQLSchema} = require('graphql')
-const user = require('../users');
+// const {
+//     GraphQLString,
+//      GraphQLList,
+//       GraphQLObjectType,
+//        GraphQLNonNull,
+//         GraphQLSchema} = require('graphql')
+// const user = require('../users');
 
-const usersType = new GraphQLObjectType({
-    name: "user",
-    fields: () => ({
-        id: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        name: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        age: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-    })
-});
+// const usersType = new GraphQLObjectType({
+//     name: "user",
+//     fields: () => ({
+//         id: {
+//             type: new GraphQLNonNull(GraphQLString)
+//         },
+//         name: {
+//             type: new GraphQLNonNull(GraphQLString)
+//         },
+//         age: {
+//             type: new GraphQLNonNull(GraphQLString)
+//         },
+//     })
+// });
 
-const usersQuery = new GraphQLObjectType({
-    name: "usersSchema",
-    fields: () => ({
-        user: {
-            type: new GraphQLList(usersType),
-            args: {
-                id: {type: GraphQLString}
-            },
-            resolve:  (root, args) => {
-                if (args.id) return user.find(u => u.id === args.id)
-                return user
-            }
-        }
-    })
-})
+// const usersQuery = new GraphQLObjectType({
+//     name: "usersSchema",
+//     fields: () => ({
+//         user: {
+//             type: new GraphQLList(usersType),
+//             args: {
+//                 id: {type: GraphQLString}
+//             },
+//             resolve:  (root, args) => {
+//                 if (args.id) return user.find(u => u.id === args.id)
+//                 return user
+//             }
+//         }
+//     })
+// })
 
-const usersMutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: () => ({
-        editUser: {
-            type: new GraphQLList(usersType),
-            args: {
-                id: {
-                    type: GraphQLString
-                },
-                name: {
-                    type: GraphQLString
-                },
-                age: {
-                    type: GraphQLString
-                }
-            },
-            resolve: function (root, args) {
-                if (args.id) {
-                    let edited = user.find(a => a.id === args.id);
-                    edited.model = args.name;
-                    return [edited]
-                }
+// const usersMutation = new GraphQLObjectType({
+//     name: "Mutation",
+//     fields: () => ({
+//         editUser: {
+//             type: new GraphQLList(usersType),
+//             args: {
+//                 id: {
+//                     type: GraphQLString
+//                 },
+//                 name: {
+//                     type: GraphQLString
+//                 },
+//                 age: {
+//                     type: GraphQLString
+//                 }
+//             },
+//             resolve: function (root, args) {
+//                 if (args.id) {
+//                     let edited = user.find(a => a.id === args.id);
+//                     edited.model = args.name;
+//                     return [edited]
+//                 }
 
-            }
-        }
-    })
-})
+//             }
+//         }
+//     })
+// })
 
-const Schema = new GraphQLSchema({
-    query: usersQuery,
-    mutation: usersMutation
-})
+// const Schema = new GraphQLSchema({
+//     query: usersQuery,
+//     mutation: usersMutation
+// })
 
 
-module.exports = Schema;
+// module.exports = Schema;
